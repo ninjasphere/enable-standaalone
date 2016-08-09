3a9d0485e3c55c0e4c8f761e56665455e0266ef7
Jon Seymour <jon@wildducktheories.com>
2016-08-09 16:37:52 +1000

    Don't enable --noCloud on devices that are also meshed.
    Signed-off-by: Jon Seymour <jon@wildducktheories.com>

--- ninjapack/root/etc/profile.d/disable-pairing.sh
+++ ninjapack/root/etc/profile.d/disable-pairing.sh
@@ -1,1 +1,4 @@
-export NINJA_SPHERE_CLIENT_ARGS=${NINJA_SPHERE_CLIENT_ARGS}${NINJA_SPHERE_CLIENT_ARGS:+ }--noCloud
+if ! test -f /data/etc/opt/ninja/mesh.json; then
+	# enable noCloud
+	export NINJA_SPHERE_CLIENT_ARGS=${NINJA_SPHERE_CLIENT_ARGS}${NINJA_SPHERE_CLIENT_ARGS:+ }--noCloud
+fi
